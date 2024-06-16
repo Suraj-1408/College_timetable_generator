1d1593b2fbf54b1783901cc4f4f6053f02cccb7e
Suraj-1408 <surajpaswanc0497@gmail.com>
2024-06-16 08:48:07 +0530

    Updated MainFlask

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from  flask import Flask,render_template
 from flask_mysqldb import MySQL
-from student import student_blp
+#from models import create_tables
 
 app = Flask(__name__,template_folder='templates')
 
@@ -11,13 +11,34 @@ app.config['MYSQL_DB'] = 'college_schedule'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
+# Call create_tables during application initialization
+#create_tables()
 
 app.secret_key = 'sadsmdsdeopmas'  # Necessary for session management
 
 
-student_blp.mysql = mysql
+def register_blueprints(app):
 
-app.register_blueprint(student_blp,url_prefix='/student')
+        from student import student_blp
+        from professor import professor_blp
+        from admin import admin_blp
+        from timetable import timetable_blp
+
+
+        # Attach the MySQL instance to the blueprint
+        student_blp.mysql = mysql
+        professor_blp.mysql = mysql
+        admin_blp.mysql = mysql
+        timetable_blp.mysql = mysql
+
+
+        app.register_blueprint(student_blp,url_prefix='/student')
+        app.register_blueprint(professor_blp,url_prefix='/professor')
+        app.register_blueprint(admin_blp,url_prefix='/admin')
+        app.register_blueprint(timetable_blp,url_prefix='/timetable')
+
+register_blueprints(app)
+
 
 @app.route('/')
 def home():
@@ -35,8 +56,6 @@ def home():
 #@app.route('/student')
 #def logout():
 #    return render_template('/student_login.html')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
