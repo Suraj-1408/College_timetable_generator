1f6ee24878cddd1e3ef4e52d5dc6521a86348350
Suraj-1408 <Surajpaswanc0497@gmail.com>
2024-06-26 09:55:36 +0530

    Updated admin with MSc & MCA url

--- admin.py
+++ admin.py
@@ -1,14 +1,14 @@
-from flask import Flask,Blueprint,render_template,request,flash,redirect,url_for,session,current_app
+from flask import Flask,flash, Blueprint, render_template, request,  redirect, url_for, session, current_app
 import MySQLdb.cursors
 import re
 
-from models import add_professor, get_all_professors, add_course, get_all_courses,add_lecture,add_classroom,get_all_classrooms,get_all_lectures,generate_valid_timetables
+from models import add_professor,add_course,gen_msc_course_id,gen_mca_course_id,add_lecture,add_classroom,get_all_classrooms,get_all_lectures,gen_professor_id
+admin_blp = Blueprint('admin', __name__, static_folder='static', template_folder='templates/admin')
 
-from forms import AddProfessorForm, AddCourseForm,AddLectureForm,AddClassroomForm
+from forms import AddProfessorForm,AddCourseForm,AddLectureForm,AddClassroomForm,AddMCACourseForm,UpdateClassroomForm
+from timetable import create_timetables_for_program
 from datetime import datetime
 #from timetable import generate_timetable,download_timetable
-
-admin_blp = Blueprint('admin',__name__,static_folder='static',template_folder='templates/admin')
 
 
 @admin_blp.route('/')
@@ -16,7 +16,7 @@ def dashboard():
     return render_template('admin_dashboard.html')
 
 
-@admin_blp.route('/admin_login',methods = ['GET','POST'])
+@admin_blp.route('/admin_login',methods = ['POST','GET'])
 def admin_login():
     mesage = ''
     mysql = admin_blp.mysql  # Access the MySQL instance
@@ -24,9 +24,7 @@ def admin_login():
     if request.method == 'POST' and 'email' in request.form and 'password' in request.form :
         email = request.form['email']
         password = request.form['password']
-        #mysql = current_app.extensions['mysql']
-        #if not mysql:
-         #   return "MySQL not initialized", 500
+            
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM admin WHERE email = %s AND password = %s', (email,password,))
         admin = cursor.fetchone()
@@ -117,64 +115,177 @@ def admin_register():
 @admin_blp.route('/add_professor', methods=['GET', 'POST'])
 def add_professor_view():
     form = AddProfessorForm()
-    if form.validate_on_submit():
-        professor_id = form.professor_id.data
-        name = form.name.data
-        add_professor(professor_id, name)
-        flash('Professor added successfully!', 'success')
-        return render_template('add_professor.html', form=form)
 
+    if form.validate_on_submit():
+        name = form.name.data
+        
+        try:
+            professor_id =  gen_professor_id()
+            if  professor_id:
+                result = add_professor(professor_id, name)
+            
+                if result:
+                    flash(f'Professor {name} added Successfully with professor Id {professor_id}','success')
+                    return redirect(url_for('admin.add_professor_view'))
+
+                else:
+                    flash(f'Professor {name} added Successfully with professor Id {professor_id}','success')
+
+            else:
+                flash('Error while generating professor Id.','danger')
+
+        except MYSQLdb.IntegrityError as e:
+            flash(f'Error: {e}','danger')
+            print(f"MySQL Integrity Error:{e}")
+
+        except Exception as e:
+            flash(f'Error adding  professor:{str(e)}','danger')
+            print(f"Exception in add_professor_view {e}")
+
+    #else:
+    #    flash('Error generating Professor Id, try again','danger')
+    #    print("Failed to generate professor Id")
+        
     return render_template('add_professor.html', form=form)
 
 
+@admin_blp.route('/delete_professor/<string:professor_id>',methods=['GET','POST'])
+def delete_professor(professor_id):
+    delete_professor_by_id(professor_id)
+    flash('Professor deleted Successfully','success')
+    return redirect(url_for('admin.list_professors'))
 
 
-@admin_blp.route('/add_course', methods=['GET', 'POST'])
-def add_course_view():
+def delete_professor_by_id(professor_id):
+    mysql = admin_blp.mysql
+    conn = mysql.connection
+    cursor = conn.cursor()
+    query = "Delete from professors where professor_id=%s"
+    cursor.execute(query,(professor_id,))
+    conn.commit()
+    cursor.close()
+
+
+
+@admin_blp.route('/update_professor/<string:professor_id>', methods=['GET', 'POST'])
+def update_professor(professor_id):
+    professor = get_professor_by_id(professor_id)
+
+    form = AddProfessorForm(name=professor['name'])  # Use the same form for adding and updating
+
+    mysql = admin_blp.mysql
+    conn = mysql.connection
+    cursor = conn.cursor()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        form_data = {
+            'name': form.name.data
+        }
+        update_professor_in_db(professor_id, form_data)
+        flash('Professor updated successfully', 'success')
+        return redirect(url_for('admin.list_professors'))
+
+    return render_template('update_professor.html', form=form, professor=professor)
+
+def get_professor_by_id(professor_id):
+    mysql = admin_blp.mysql
+    conn = mysql.connection
+    cursor = conn.cursor()
+    query = "SELECT professor_id, name FROM professors WHERE professor_id = %s"
+    cursor.execute(query,(professor_id,))
+    result = cursor.fetchone()
+    cursor.close()
+    return result
+   
+
+def update_professor_in_db(professor_id, form_data):
+    mysql = admin_blp.mysql
+    conn = mysql.connection
+    cursor = conn.cursor()
+    query = "UPDATE professors SET name = %s WHERE professor_id = %s"
+    cursor.execute(query, (form_data['name'], professor_id))
+    conn.commit()
+    cursor.close()
+
+
+
+@admin_blp.route('/add_msc_course',methods=['GET','POST'])
+def add_msc_course():
     form = AddCourseForm()
 
-    # Fetch professors from the database
-    mysql = admin_blp.mysql  # Access the MySQL instance
-    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute("SELECT professor_id, name FROM professors")
+    mysql = admin_blp.mysql
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT  name FROM professors")
     professors = cursor.fetchall()
     cursor.close()
 
-    # Populate the professors field
-    form.professors.choices = [(professor['professor_id'], professor['name']) for professor in professors]
+    form.professor_name.choices = [(professor['name'], professor['name']) for professor in professors]
+
 
     if form.validate_on_submit():
-        course_number = form.course_number.data
         course_name = form.course_name.data
-        max_numb_students = form.max_numb_students.data
-        selected_professors = form.professors.data
+        professor_name = form.professor_name.data
+        total_credits = form.total_credits.data
+        no_of_lectures = form.no_of_lectures.data
 
-        cursor = mysql.connection.cursor()
-        cursor.execute(
-            "INSERT INTO courses (course_number, course_name, max_numb_students) VALUES (%s, %s, %s)",
-            (course_number, course_name, max_numb_students)
-        )
-        mysql.connection.commit()
+        try:
+            course_id = gen_msc_course_id()
+            if course_id:
+                cursor = mysql.connection.cursor()
+                query = "Insert into msc_courses(course_id,course_name,professor_name,total_credits,no_of_lectures) values(%s,%s,%s,%s,%s)" 
+                cursor.execute(query,(course_id,course_name,professor_name,total_credits,no_of_lectures))
+                mysql.connection.commit()
+                cursor.close()   
+                flash(f'{course_name} added successfully with ID {course_id}','success')
+                return redirect(url_for('admin.add_msc_course'))
+                #return render_template('add_msc_course.html',form=form,course_type='MSC')
 
-        # Retrieve the course_id of the newly added course
-        cursor.execute("SELECT course_id FROM courses WHERE course_number = %s", (course_number,))
-        course_id = cursor.fetchone()['course_id']
+            else:
+                flash(f'Course Id not generated','danger')
 
-        # Insert into course_professor table
-        for professor_id in selected_professors:
-            cursor.execute(
-                "INSERT INTO course_professor (course_id, professor_id) VALUES (%s, %s)",
-                (course_id, professor_id)
-            )
+        except Exception as e:
+            flash(f'Error while adding MSc course {e}','danger')
 
-        mysql.connection.commit()
-        cursor.close()
+    return render_template('add_msc_course.html',form=form,course_type='MSC')
 
-        flash('Course added successfully!', 'success')
-       #return redirect(url_for('admin.add_course_view'))
-        return render_template('add_course.html', form=form)
-        
-    return render_template('add_course.html', form=form)
+
+@admin_blp.route('/add_mca_course',methods = ['GET','POST'])
+def add_mca_course():
+    form = AddCourseForm()
+
+    mysql = admin_blp.mysql
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT  name FROM professors")
+    professors = cursor.fetchall()
+    cursor.close()
+
+    form.professor_name.choices = [(professor['name'], professor['name']) for professor in professors]
+
+
+    if form.validate_on_submit():
+        course_name = form.course_name.data
+        professor_id = form.professor_name.data
+        total_credits = form.total_credits.data
+        no_of_lectures = form.no_of_lectures.data
+
+        try:
+            course_id = gen_mca_course_id()
+            if course_id:
+                cursor = mysql.connection.cursor()
+                query = "Insert into mca_courses(course_id,course_name,professor_name,total_credits,no_of_lectures) values(%s,%s,%s,%s,%s)"
+                cursor.execute(query,(course_id,course_name,professor_id,total_credits,no_of_lectures))
+                mysql.connection.commit()
+                cursor.close()
+                flash(f'{course_name } added successfully with ID {course_id}','success')
+                return redirect(url_for('admin.add_mca_course'))
+            
+            else:
+                flash('Course Id not added','danger')
+            
+        except Exception as e:
+            flash(f'Error while adding MCA course {e}','danger')
+
+    return render_template('add_mca_course.html',form=form,course_type='MCA')
 
 
 
@@ -192,10 +303,28 @@ def list_professors():
     #professors = get_all_professors()
     return render_template('professors.html', professors=professors)
 
-@admin_blp.route('/courses')
-def list_courses():
-    courses = get_all_courses()
-    return render_template('courses.html', courses=courses)
+@admin_blp.route('/msc_courses')
+def list_msc_courses():
+    mysql = admin_blp.mysql
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    
+    cursor.execute("Select * from msc_courses")
+    msc_courses = cursor.fetchall()
+    cursor.close()
+
+    return render_template('msc_courses.html', courses=msc_courses)
+
+@admin_blp.route('/mca_courses')
+def list_mca_courses():
+    mysql = admin_blp.mysql
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+    cursor.execute("Select * from mca_courses")
+    mca_courses = cursor.fetchall()
+    cursor.close()
+
+    return render_template('mca_courses.html',courses=mca_courses)
+    
 
 
 
@@ -285,24 +414,17 @@ def add_classroom_view():
     form = AddClassroomForm()
 
     if form.validate_on_submit():
-        classroom_name = form.classroom_name.data
+        #classroom_name = form.classroom_name.data
         classroom_capacity = form.classroom_capacity.data
-        add_classroom(classroom_name,classroom_capacity)
-        flash('ClassRoom added Sucessfully !','success')
-        return render_template('add_classroom.html',form=form)
+        try:
+            add_classroom(classroom_capacity)
+            flash('ClassRoom added Sucessfully !','success')
+            return redirect(url_for('admin.add_classroom_view'))
+        
+        except Exception as e:
+            flash(f'Error while adding classroom {e}','danger')
 
     return render_template('add_classroom.html',form=form)
-
-
-def add_classroom(classroom_name,classroom_capacity):
-    mysql=admin_blp.mysql
-    conn = mysql.connection
-    cursor = conn.cursor()
-    query = "Insert into classrooms (classroom_name,classroom_capacity) values (%s,%s)"
-    cursor.execute(query,(classroom_name,classroom_capacity))
-    conn.commit()
-    cursor.close()
-
 
 
 @admin_blp.route('/classrooms')
@@ -311,11 +433,14 @@ def list_classrooms():
     return render_template('classrooms.html', classrooms=classrooms)
 
 
-@admin_blp.route('/delete_classroom/<int:classroom_id>', methods = ['POST'])
+@admin_blp.route('/delete_classroom/<classroom_id>', methods = ['POST'])
 def delete_classroom(classroom_id):
-    delete_classroom_by_id(classroom_id)
-    flash('Classroom deleted Sucessfully!','succes')
-    #return render_template('classrooms.html')
+    try:
+        delete_classroom_by_id(classroom_id)
+        flash('Classroom deleted Sucessfully!','succes')
+        #return render_template('classrooms.html')
+    except Exception as e:
+        flash(f'Error while deleting classroom:{e}','danger')
     return redirect(url_for('admin.list_classrooms'))  # Redirect to classrooms page after deletion
 
 
@@ -329,4 +454,60 @@ def delete_classroom_by_id(classroom_id):
     cursor.close()
 
 
-  
+
+@admin_blp.route('/update_classroom/<classroom_id>', methods=['GET', 'POST'])
+def update_classroom(classroom_id):
+    form = AddClassroomForm()  # Use the same form for adding and updating
+
+    mysql = admin_blp.mysql
+    conn = mysql.connection
+    cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+
+    if request.method == 'GET':
+        # Fetch existing classroom details to populate the form
+        cursor.execute("SELECT * FROM classrooms WHERE classroom_id = %s", (classroom_id,))
+        classroom = cursor.fetchone()
+        cursor.close()
+
+        if classroom:
+            form.classroom_id.data = classroom['classroom_id']
+            #form.classroom_name.data = classroom['classroom_name']
+            form.classroom_capacity.data = classroom['classroom_capacity']
+        else:
+            flash('Classroom not found!', 'danger')
+            return redirect(url_for('admin.list_classrooms'))
+
+    if form.validate_on_submit():
+        classroom_id = form.classroom_id.data 
+        #classroom_name = form.classroom_name.data
+        classroom_capacity = form.classroom_capacity.data
+
+        cursor = conn.cursor()
+        query = "UPDATE classrooms SET classroom_id = %s, classroom_capacity = %s WHERE classroom_id = %s"
+        cursor.execute(query, (classroom_id,classroom_capacity,classroom_id))
+        conn.commit()
+        cursor.close()
+
+        flash('Classroom updated successfully!', 'success')
+        return redirect(url_for('admin.list_classrooms'))
+
+    return render_template('update_classroom.html', form=form)
+
+
+
+def update_classroom(classroom_id,classroom_capacity):
+    mysql = admin_blp.mysql
+    conn = mysql.connection
+    cursor = conn.cursor()
+    query = "Update classrooms set classroom_capacity=%s where classroom_id=%s"
+    cursor.execute(query,(classroom_capacity,classroom_id))
+    conn.commit()
+    cursor.close()
+    
+@admin_blp.route('/generate_msc_timetable')
+def generate_msc_timetable():
+    return redirect(url_for('timetable.generate_msc_timetable'))
+
+@admin_blp.route('/generate_mca_timetable')
+def generate_mca_timetable():
+    return redirect(url_for('timetable.generate_mca_timetable'))
